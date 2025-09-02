2a30eb4ca0f5ab30b57a50433a3a80c794c71a68
Raihan <raihanuddinahmed10@gmail.com>
2025-09-02 23:22:55 +0600

    single book

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -2,15 +2,18 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 const BookDetail = () => {
   const { bookId } = useParams();
-  const id = parseInt(bookId)
   const data = useLoaderData();
+  const id = parseInt(bookId);
 
   const book = data.find((book) => book.bookId === id);
-  console.log(data, book);
+  console.log(book);
 
   return (
-    <div>
+    <div className="my-5">
       <h2>BookDetail: {bookId}</h2>
+      <img src={image} alt={bookName} /> <br />
+      <button className="btn btn-primary">Read</button>
+      <button className="btn btn-secondary">Wish List</button>
     </div>
   );
 };

bcabb760a01eb177ba57ba2d042584965ed96584
Raihan <raihanuddinahmed10@gmail.com>
2025-09-02 22:17:29 +0600

    first commit

---
+++ src/components/Books/Books.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Books = () => {
+  return (
+    <div>
+      <h2 className="font-semibold text-5xl text-center">Books</h2>
+    </div>
+  );
+};
+
+export default Books;
---
+++ src/main.jsx
@@ -0,0 +1,32 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Root from "./components/Root/Root";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Home from "./components/Home/Home";
+import Dashboard from "./components/Dashboard/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path: '/',
+        element: <Home/>
+      },
+      {
+        path: 'dashboard',
+        element: <Dashboard/>
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
